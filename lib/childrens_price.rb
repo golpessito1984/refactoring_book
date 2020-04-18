e5787baecf64bea5172a7a604867da7056712e71
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-04-18 14:11:18 +0200

    Set frequent_renter_points to RegularPrice, NewReleasePrice, ChildrenPrice

--- lib/childrens_price.rb
+++ lib/childrens_price.rb
@@ -3,4 +3,8 @@ class ChildrensPrice
     result = 1.5
     result+= (days_rented-3)*1.5 if days_rented > 3
   end
+
+  def frequent_renter_points(days_rented)
+   1
+  end
 end
--- lib/regular_price.rb
+++ lib/regular_price.rb
@@ -4,4 +4,9 @@ class RegularPrice
     result+= (days_rented)*1.5 if days_rented > 2
     return result
   end
+
+  def frequent_renter_points(days_rented)
+   1
+  end
+  
 end
